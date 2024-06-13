0bf04483108304e3a43ac597abf3d25599dc9f6f
Safa Aslam <160883400+safaaslam96@users.noreply.github.com>
2024-06-13 16:40:14 +0500

    pushcode

---
+++ README.md
@@ -0,0 +1,2 @@
+# Todo-List
+This is my to-do-list
---
+++ index.ts
@@ -0,0 +1,65 @@
+import inquirer from "inquirer";
+
+let todos: string[] = [];
+
+let addTask;
+
+do {
+
+     addTask = await inquirer.prompt(
+    {
+    name: "task",
+    type: "confirm",
+    message: "Do you want to add any task?",
+    }
+  );
+  
+if (addTask.task) {
+  let doTask = await inquirer.prompt({
+    name: "taskToAdd",
+    type: "input",
+    message: "Please type your task here:",
+  });
+  todos.push(doTask.taskToAdd);
+  
+} else {
+      let addTask;
+    do{
+       addTask = await inquirer.prompt(
+        {
+              name: "delete",
+              type: "confirm",
+              message: "Do you want to delete your task?",
+              }
+        );
+      if (addTask.delete) {
+        let deleteQues = await inquirer.prompt(
+          {
+           type:"list",
+           choices:todos,
+           message:"Select any task to delete:",
+           name:"deleteTask"
+           }
+        );
+        
+        if (todos.includes(deleteQues.deleteTask))
+          {
+            let i: number = todos.indexOf(deleteQues.deleteTask);
+            todos.splice(i , 1);
+            console.log("Your task has been deleted.");
+            
+          }
+      }
+
+    } while(addTask.delete);
+   
+}
+  
+    console.log("Here is your todo list:");
+    for (let todo of todos)
+      {
+        console.log(todo);
+        
+      }
+}while (addTask.task)
+
